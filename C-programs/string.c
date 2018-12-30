ce83bd9825b6319ef7b54792372ccaf934d3315f
Rohan Kumar <f20181013@goa.bits-pilani.ac.in>
2018-12-30 16:51:12 +0530

    second

--- C-programs/string.c
+++ C-programs/string.c
@@ -1,7 +1,7 @@
 #include<stdio.h>  
 void main ()  
 {  
-    char s[20];  
+    char s[20];  //test
     printf("Enter the string?");  
     scanf("%[^\n]s",s);  
     printf("You entered %s",s);  
